2fabdb76bb6701dd1dfe9d67ee45c86388cdfad7
Friosn <franz.rios.nino@gmail.com>
2022-11-23 20:05:47 +0100

    bug: resolved eslint bug; files equalized

--- server/src/domain/entities/entity-class.js
+++ server/src/domain/entities/entity-class.js
@@ -1,17 +1,16 @@
 
-
 module.exports = (db) => {
-    const classSchema = new db.Schema({
-        name: {type: String, required: true},
-        reproduccion: {type: String, required: true},
-        incuvation: {type: String, required: true},
-        skeletonType: {type: String, required: true},
-        skinType: {type: String, required: true},
-        orders: [{type: db.Schema.Types.ObjectId, ref: "order", required: true}]
-    },
-    {
-        timestamps: true,
-    }
-    );
-    return db.model("class", classSchema);
+  const classSchema = new db.Schema({
+    name: { type: String, required: true },
+    reproduccion: { type: String, required: true },
+    incuvation: { type: String, required: true },
+    skeletonType: { type: String, required: true },
+    skinType: { type: String, required: true },
+    orders: [{ type: db.Schema.Types.ObjectId, ref: 'order', required: true }]
+  },
+  {
+    timestamps: true
+  }
+  )
+  return db.model('class', classSchema)
 }

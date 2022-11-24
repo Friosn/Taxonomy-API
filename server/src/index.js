32f0c01b42b303d057238398a96377925ee40e5e
Friosn <franz.rios.nino@gmail.com>
2022-11-24 15:48:41 +0100

    user model

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 
-const app = express();
+const { connect } = require('./helper/database/connect')
+
+connect()
+
+const app = express()
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH')
   res.header('Access-Control-Allow-Credentials', true)
@@ -19,3 +24,20 @@ app.use(
 // We also want a transition- limit in our app; so... 1mb is a lot, we normally will put less
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({ limit: '1mb', extended: true }))
+app.set('secretKey', process.env.SECRET_KEY_JWT) // we will normally delete the one assigned at the beggining and put directly this one
+// ----------Here will come the routes of the server--------
+
+// --------------------------------
+
+app.use((error, req, res, next) => {
+  return res
+    .status(error.status || 500)
+    .json(error.message || 'Unexpected error 🤢')
+})
+
+app.disable('x-powered-by') // Like that anybody will be able to know with which lenguage was this app made
+
+// Now we connect to the server
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port : http://localhost:${process.env.PORT}`) // this PORT would also be directly substituted by process.env.PORT for more anonymity
+})
